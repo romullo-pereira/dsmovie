cd29d694f9bc298b26d1346d397ba47ee95cce2e
romullo-pereira <romulo.pereira@bandtec.com.br>
2022-03-09 00:30:26 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,36 @@
+
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { Fragment } from "react";
 
 function Listing() {
-    return(
-        <Pagination />
+    return (
+        <Fragment>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     );
 }
 
