34fd43c129f10e7b7c5ebdcb50c98192fa4a89a3
romullo-pereira <romulo.pereira@bandtec.com.br>
2022-03-12 17:25:48 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,18 @@
 
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { Fragment } from "react";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies`)
+        .then(response => {
+            console.log(response.data);
+            
+        });
+
     return (
         <Fragment>
             <Pagination />
