3b8dc89096c53fa8b253c4b8754255fe94bc894d
romullo-pereira <romulo.pereira@bandtec.com.br>
2022-03-12 17:58:23 -0300

    props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -10,42 +10,37 @@ function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
 
-    useEffect(() => {
-        axios.get(`${BASE_URL}/movies?page=1`)
-        .then(response => {
-            const data = response.data as MoviePage;
-            console.log(data);
-            setPageNumber(data.number);
-        });
-    }, [])
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true,
+    });
 
-    
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?page=${pageNumber}&size=12&sort=id`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber])
 
     return (
         <Fragment>
-            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-3 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard  movie={movie} />
+                        </div>
+                    ))}
 
-                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-3 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
                 </div>
             </div>
         </Fragment>
