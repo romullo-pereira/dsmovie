02143d017c53bffb2be8905ee4757b6a5ef4decb
romullo-pereira <romulo.pereira@bandtec.com.br>
2022-03-12 17:38:11 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,19 +2,28 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?page=1`)
         .then(response => {
-            console.log(response.data);
-            
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
         });
+    }, [])
+
+    
 
     return (
         <Fragment>
+            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
